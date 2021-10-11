83a070f81335c01f5af175106316f766c46b2f3c
SergioAldana <saaldana2@misena.edu.co>
2021-10-11 17:59:14 -0500

    chore: se modifica la clase principal app

--- Parcial/src/co/edu/utadeo/App.java
+++ Parcial/src/co/edu/utadeo/App.java
@@ -1,9 +1,13 @@
 package co.edu.utadeo;
 
+import java.util.Scanner;
+
 public class App {
 
     // Driver Program
     public static void main(String[] args) {
+
+        int N;
 
         /* Let us create following weighted ejercicioTres
                  10
@@ -13,36 +17,61 @@ public class App {
             |      \ |
             2--------3
                 4       */
-        int V = 4;  // Number of vertices in ejercicioTres
-        int E = 5;  // Number of edges in ejercicioTres
-        EjercicioTres ejercicioTres = new EjercicioTres(V, E);
+        //Ciclos del programa
+        System.out.println("Numero de veces que quiere que se ejecute el programa: ");
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
 
-        // add edge 0-1
-        ejercicioTres.edge[0].src = 0;
-        ejercicioTres.edge[0].dest = 1;
-        ejercicioTres.edge[0].weight = 10;
+        for (int i = 0; i < N; i++) {
 
-        // add edge 0-2
-        ejercicioTres.edge[1].src = 0;
-        ejercicioTres.edge[1].dest = 2;
-        ejercicioTres.edge[1].weight = 6;
+            int V;
+            int E;
 
-        // add edge 0-3
-        ejercicioTres.edge[2].src = 0;
-        ejercicioTres.edge[2].dest = 3;
-        ejercicioTres.edge[2].weight = 5;
+            // Number of vertices in ejercicioTres
+            System.out.println("Numero de esferas del dragon: ");
+            Scanner scannerUno = new Scanner(System.in);
+            V = scannerUno.nextInt();
 
-        // add edge 1-3
-        ejercicioTres.edge[3].src = 1;
-        ejercicioTres.edge[3].dest = 3;
-        ejercicioTres.edge[3].weight = 15;
+            // Number of edges in ejercicioTres
+            System.out.println("Numero de intersecciones de las ubicaciones: ");
+            Scanner scannerDos = new Scanner(System.in);
+            E = scannerDos.nextInt();
 
-        // add edge 2-3
-        ejercicioTres.edge[4].src = 2;
-        ejercicioTres.edge[4].dest = 3;
-        ejercicioTres.edge[4].weight = 4;
+            //int V = 4;  // Number of vertices in ejercicioTres
+            //int E = 5;  // Number of edges in ejercicioTres
 
-        System.out.println(ejercicioTres.KruskalMST());
+            EjercicioTres ejercicioTres = new EjercicioTres(V, E);
+
+            for (int j = 0; j < E; j++) {
+
+                // add edge 0-1
+                ejercicioTres.edge[0].src = 0;
+                ejercicioTres.edge[0].dest = 1;
+                ejercicioTres.edge[0].weight = 10;
+
+                // add edge 0-2
+                ejercicioTres.edge[1].src = 0;
+                ejercicioTres.edge[1].dest = 2;
+                ejercicioTres.edge[1].weight = 6;
+
+                // add edge 0-3
+                ejercicioTres.edge[2].src = 0;
+                ejercicioTres.edge[2].dest = 3;
+                ejercicioTres.edge[2].weight = 5;
+
+                // add edge 1-3
+                ejercicioTres.edge[3].src = 1;
+                ejercicioTres.edge[3].dest = 3;
+                ejercicioTres.edge[3].weight = 15;
+
+                // add edge 2-3
+                ejercicioTres.edge[4].src = 2;
+                ejercicioTres.edge[4].dest = 3;
+                ejercicioTres.edge[4].weight = 4;
+
+                System.out.println(ejercicioTres.KruskalMST());
+            }
+        }
     }
 
 }
